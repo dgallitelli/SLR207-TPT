4d7af5bd29632ae26b45ae49b70c2d6005e5ea42
Davide Gallitelli <davidegallitelli@gmail.com>
2018-06-06 14:28:29 +0200

    Working on step 49 - Slave Reduce

--- SLAVE/src/Slave.java
+++ SLAVE/src/Slave.java
@@ -1,27 +1,44 @@
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class Slave {
 
+    private int opCode;
     private String fileToMap;
+    private List<String> UMFiles;
+    private String reduceKey;
+    private String outputFile;
     private static String rootFolder = "/tmp/dgallitelli/";
 
-    public Slave(String fileToMap) {
-        this.fileToMap = fileToMap;
+    public Slave(String[] args) {
+        this.opCode = Integer.parseInt(args[0]);
+        if (this.opCode == 0){
+            // Mode mapping
+            this.fileToMap = args[1];
+        } else {
+            this.reduceKey = args[1];
+            this.outputFile = args[2];
+            this.UMFiles = new ArrayList<>();
+            for (int i = 3; i<args.length; i++)
+                this.UMFiles.add(args[i]);
+        }
     }
 
     public static void main(String[] args) {
 
-        if (args.length < 1){
+        if (args.length < 2){
             System.out.println("Not enough args.");
             return;
         }
 
-        Slave sl = new Slave(args[0]);
+        Slave sl = new Slave(args);
         //sl.map();
-        sl.reduce();
+        //sl.map2();
+        try {
+            sl.reduce();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private void map() {
@@ -58,7 +75,7 @@ public class Slave {
         }
     }
 
-    private void reduce(){
+    private void map2(){
         // Get the file number
         String[] items = fileToMap.split("/");
         String numberOfFile = items[items.length-1].split("")[1];
@@ -95,6 +112,34 @@ public class Slave {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void reduce() throws IOException {
+        // Get the file to write ready
+        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(this.outputFile)));
+        // Variables for file read
+        FileReader fr;
+        BufferedReader br;
+        Scanner sc;
+        String line;
+        // For every file in UMFiles
+        for (String UM : this.UMFiles){
+            fr = new FileReader(UM);
+            br = new BufferedReader(fr);
+            sc = new Scanner(br);
+            // Read a line and update the map
+            while (sc.hasNextLine()){
+                line = sc.nextLine();
+                if (this.reduceKey.equals(line.split(" ")[0])){
+                    // Write on output file a new line
+                    out.write(line);
+                }
+            }
+            sc.close();
+            br.close();
+            fr.close();
+        }
+        out.close();
     }
 
     /**
